a66421b9825401e79a645941e23002d3f5a07dca
Mrrebrik <itibsoft@gmail.com>
2021-01-14 21:59:58 +0300

    create project

---
+++ Unity-Contours-game/Assets/Project/Scripts/Cards.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cards : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public int id;
+    [SerializeField] private RectTransform groupe;
+
+    [SerializeField] private Canvas canvas;
+    [HideInInspector] public RectTransform rectCard = new RectTransform();
+    [HideInInspector] public Vector3 startPosition;
+
+    private void Awake()
+    {
+        rectCard = GetComponent<RectTransform>();
+        startPosition = rectCard.position;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        rectCard.anchoredPosition += eventData.delta / canvas.scaleFactor / groupe.localScale;
+    }
+}
---
+++ Unity-Contours-game/Assets/Project/Scripts/Cells.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cells : MonoBehaviour, IDropHandler
+{
+    [SerializeField] private int id;
+    private Vector2 positionCell;
+
+    private void Awake()
+    {
+        positionCell = GetComponent<RectTransform>().anchoredPosition;
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        Cards _card = eventData.pointerDrag.GetComponent<Cards>();
+        Debug.Log("Droping");
+        if(id == _card.id)
+        {
+            _card.rectCard.anchoredPosition = positionCell;
+            _card.enabled = false;
+        }
+        else
+        {
+            Debug.Log("TEST");
+        }
+    }
+}
